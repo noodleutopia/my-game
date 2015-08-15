feddb09e34ed0dd370a5848728fb981cff268d96
noodleutopia <noodleutopia@outlook.com>
2015-08-16 01:40:05 +0800

    增加了难度选择，以选择不同AI策略。

--- app/src/main/java/pku/ss/zyf/gameView/GameView.java
+++ app/src/main/java/pku/ss/zyf/gameView/GameView.java
@@ -14,13 +14,11 @@ import pku.ss.zyf.mygame_2q48.GamePlay;
  */
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private FlushThread thread = null;
     private GamePlay gamePlay;
 
     public GameView(Context context) {
         super(context);
         this.getHolder().addCallback(this);
-        thread = new FlushThread(this.getHolder(), this);
         gamePlay = new GamePlay();
     }
 
@@ -42,44 +40,5 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-    }
-
-    class FlushThread extends Thread {
-        private boolean flag = false;
-        private final int span = 500;
-        private final GameView gameView;
-        private final SurfaceHolder holder;
-
-        public FlushThread(SurfaceHolder holder, GameView gameView) {
-            this.gameView = gameView;
-            this.holder = holder;
-        }
-
-        @Override
-        public void run() {
-            Canvas canvas;
-            while (this.flag) {
-                canvas = null;
-                try {
-                    canvas = this.holder.lockCanvas(null);
-                    synchronized (this.holder) {
-                        this.gameView.onDraw(canvas);
-                    }
-                } finally {
-                    if (canvas != null) {
-                        this.holder.unlockCanvasAndPost(canvas);
-                    }
-                }
-
-                try {
-                    Thread.sleep(span);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        ;
-
     }
 }
